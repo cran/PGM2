ab227f729ae37b212bf308788e2008f82ff8e214
Mohamed Laib <laib.med@gmail.com>
2016-12-17 21:23:52 +0000

    version 1.0-1

--- R/Uniform.R
+++ R/Uniform.R
@@ -1,12 +1,45 @@
-Uniform <-
-function(mat){
-w<-mat
-v <- sort(unique(as.vector(w)))
-W <- w
-malist <- NULL
-TRI <- function(a,b) all(a %in% b)
+#' Uniform designs
+#'
+#' Builds the uniform design (UD) using a Resolvable Balanced Incomplete
+#' Block Design (RBIBD)
+#'
+#' @usage Uniform(mat)
+#' @param mat The matrix of the RBIBD.
+#' @return A LIST of:
+#' \enumerate{
+#'  \item \code{n }{Number of experiments.}
+#'  \item \code{F }{The dimension of the design.}
+#'  \item \code{UD }{The configuration of the uniform design.}
+#' }
+#' @author Mohamed Laib, Abla Boudraa and Zebida Gheribi-Aoulmi
+#' @references Fang.K.T et al., Constructions of uniform designs by
+#' using resolvable packings and coverings. Discrete Math. (19), 2003, 692-711.
+#'
+#' Fang.K.T et al., Construction of uniform designs via super-simple
+#' resolvable t-designs. Util. Math. (66).2004, 15-32.
+#' @examples
+#' \dontrun{
+#'
+#' m<-4
+#' X<-BIB(m)
+#'
+#' n<-1
+#' mat<-X$BIB
+#' Y<-Resolvable(n,mat)
+#'
+#' ud<-Uniform(Y$RBIB)  #Uniform design
+#' }
+#' @importFrom stats na.omit
+#' @export
 
-k <- 1
+Uniform <-function(mat){
+  w<-mat
+  v <- sort(unique(as.vector(w)))
+  W <- w
+  malist <- NULL
+  TRI <- function(a,b) all(a %in% b)
+
+  k <- 1
 
 while (TRUE) {
   vv <- v
@@ -31,8 +64,11 @@ while (TRUE) {
 }
 x<-Reduce("rbind",malist)
 
-##
-a<-max(x);b<-length(malist);c<-dim(x)[1];lev<-c/b;ex<-length(v)
+a<-max(x)
+b<-length(malist)
+c<-dim(x)[1]
+lev<-c/b
+ex<-length(v)
 UD<-matrix(nrow=a,ncol=b)
 v <- sort(unique(as.vector(x)))
 for (i in 1:b) {q<-malist[[i]]
@@ -42,5 +78,6 @@ if(e[j]>lev) {e[j]<-(e[j]%%lev)}
 if(e[j]==0) {e[j]<-lev}}
 UD[,i]<-e}
 ud<-na.omit(UD)
-N<-dim(ud)[1];F<-dim(ud)[2]
+N<-dim(ud)[1]
+F<-dim(ud)[2]
 return(list(n=N,F=F,UD=ud))}
